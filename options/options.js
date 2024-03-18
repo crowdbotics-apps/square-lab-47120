16f06577a9135721ae944c5e429d442380671949
Crowdbotics <team@crowdbotics.com>
2024-03-18 15:31:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-lab-47120",
+  url: "https://square-lab-47120.botics.co",
+  api: "https://square-lab-47120.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
